6566ccd6795884b0e3e6896697a16aa73ca7cc7c
Fernando Mauro <fermaurolf@gmail.com>
2024-09-25 16:43:21 -0600

    First prototype for the computer, the windows may be customizable, but not know, and added temporal button for go to the computer scene

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -21,3 +21,7 @@ func _on_options_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_computer_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/computer.tscn")
