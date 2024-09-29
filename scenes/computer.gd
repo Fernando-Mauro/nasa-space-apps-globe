3b41b510fad4f9f15ca8e77fe2a34d8c6de31cca
Fernando Mauro <fermaurolf@gmail.com>
2024-09-29 16:50:06 -0600

    Click to glosary functional B)

--- scenes/computer.gd
+++ scenes/computer.gd
@@ -5,8 +5,22 @@ func _ready() -> void:
 	var currentTime = Time.get_time_dict_from_system()
 	var textTime = str(currentTime.hour) + ":" + str(currentTime.minute)
 	$Panel/Time/TimeLabel.text = textTime
+	
+	
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_glosary_pressed() -> void:
+	var size = get_parent_area_size() - Vector2(100,100)
+	
+	$GlosaryWindow.size = size
+	$GlosaryWindow.show()
+
+
+func _on_glosary_window_close_requested() -> void:
+	$GlosaryWindow.hide()
