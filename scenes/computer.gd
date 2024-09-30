16269a17e435dba8d8540505f1680218253b1d75
Fernando Mauro <fermaurolf@gmail.com>
2024-09-30 12:37:57 -0600

    No sé xD

--- scenes/computer.gd
+++ scenes/computer.gd
@@ -1,26 +1,28 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var currentTime = Time.get_time_dict_from_system()
 	var textTime = str(currentTime.hour) + ":" + str(currentTime.minute)
-	$Panel/Time/TimeLabel.text = textTime
-	
-	
+	$Panel/Time/TimeLabel.text = textTime	
 	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-
-func _on_glosary_pressed() -> void:
-	var size = get_parent_area_size() - Vector2(100,100)
+	if Input.is_action_just_pressed("ui_up"):
+		$GlobeWindow/CenterContainer/VideoStreamPlayer.play()
 	
+func _on_glosary_pressed() -> void:	
+	var size = DisplayServer.window_get_size()
 	$GlosaryWindow.size = size
 	$GlosaryWindow.show()
 
-
 func _on_glosary_window_close_requested() -> void:
 	$GlosaryWindow.hide()
+
+func _on_globe_pressed() -> void:
+	var size = get_parent_area_size() - Vector2(100,100)
+	$GlobeWindow.size = size
+	$GlobeWindow.show()
+	#$GlobeWindow/CenterContainer/VideoStreamPlayer.play()
+
+func _on_globe_window_close_requested() -> void:
+	$GlobeWindow.hide()
