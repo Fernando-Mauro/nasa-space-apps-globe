3f7a67e46a2392f91678b3411bd0d1a604fd6e3a
Biniza Ruiz <binizacruuiz@gmail.com>
2024-10-05 19:05:32 -0600

    glosary

--- scenes/title.gd
+++ scenes/title.gd
@@ -1,18 +1,15 @@
-extends Control
+extends Label
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_on_visibility_changed()
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-
 
 func _on_visibility_changed() -> void:
 	var selectedDay = "GLOSARY_" + GlobalComputer.selected;
-	$MarginContainer/VBoxContainer/Label.text = tr(selectedDay)
+	$MarginContainer/Label.text = tr(selectedDay)
 	
